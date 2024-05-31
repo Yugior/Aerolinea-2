4b0ad542d35a3bb6d8f003598d7811f8ac4a4212
Yugior <150100500+Yugior@users.noreply.github.com>
2024-05-31 02:07:40 -0600

    Update Asiento.h
    Purga de codigo

--- Asiento.h
+++ Asiento.h
@@ -1,27 +1,10 @@
 #ifndef ASIENTO_H
 #define ASIENTO_H
 
-#include <string>
-
-using namespace std;
-
-// Declaración adelantada de las clases Trabajo y Premier
-class Trabajo;
-class Premier;
-
 class Asiento {
-protected:
-    double costo;
-
 public:
-    // Constructor por defecto
-    Asiento();
-
-    // Método virtual para elegir asiento
+    virtual ~Asiento() {}
     virtual void elegirAsiento(double costo) = 0;
-
-    // Getter
-    double getCosto() const;
 };
 
 #endif // ASIENTO_H
