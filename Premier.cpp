74e27f9a8657099055d6850acd478887febc6ab5
Yugior <150100500+Yugior@users.noreply.github.com>
2024-06-07 03:20:59 -0600

    Update Premier.cpp
    Codigo comentado

--- Premier.cpp
+++ Premier.cpp
@@ -2,24 +2,30 @@
 
 using namespace std;
 
+// Constructor por defecto
 Premier::Premier() : masaje(""), comida("") {}
 
+// Obtiene el masaje disponible en el asiento premier
 string Premier::getMasaje() const {
     return masaje;
 }
 
+// Establece el masaje disponible en el asiento premier
 void Premier::setMasaje(const string& nuevoMasaje) {
     masaje = nuevoMasaje;
 }
 
+// Obtiene la comida disponible en el asiento premier
 string Premier::getComida() const {
     return comida;
 }
 
+// Establece la comida disponible en el asiento premier
 void Premier::setComida(const string& nuevaComida) {
     comida = nuevaComida;
 }
 
+//No hay una implementación necesaria por lo que queda vaica
 void Premier::elegirAsiento(double costo) {
-   
+    
 }
