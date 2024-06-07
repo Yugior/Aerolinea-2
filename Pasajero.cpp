0d3678869905899a4ae186b18148a460c631e266
Yugior <150100500+Yugior@users.noreply.github.com>
2024-06-07 03:19:40 -0600

    Update Pasajero.cpp
    Codigo comentado

--- Pasajero.cpp
+++ Pasajero.cpp
@@ -2,16 +2,20 @@
 #include "Trabajo.h"
 #include "Premier.h"
 
+// Constructor por defecto
 Pasajero::Pasajero() : asiento(nullptr) {}
 
+// Establece el nombre del pasajero
 void Pasajero::setNombre(const string& nombre) {
     this->nombre = nombre;
 }
 
+// Establece el teléfono del pasajero
 void Pasajero::setTelefono(const string& telefono) {
     this->telefono = telefono;
 }
 
+// Registra el pasajero con un tipo de asiento (Trabajo o Premier)
 void Pasajero::registrarPasajero(bool esTrabajo) {
     if (esTrabajo) {
         asiento = new Trabajo();
@@ -20,6 +24,7 @@ void Pasajero::registrarPasajero(bool esTrabajo) {
     }
 }
 
+// Obtiene el asiento del pasajero
 Asiento* Pasajero::getAsiento() const {
     return asiento;
 }
