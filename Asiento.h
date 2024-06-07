1aba76072d44b9e4e93d1894e5abdf069219d58a
Yugior <150100500+Yugior@users.noreply.github.com>
2024-06-07 03:18:34 -0600

    Update Asiento.h
    Se comento el codigo

--- Asiento.h
+++ Asiento.h
@@ -1,9 +1,11 @@
 #ifndef ASIENTO_H
 #define ASIENTO_H
 
+// Clase base abstracta para los asientos
 class Asiento {
 public:
     virtual ~Asiento() {}
+    // Función virtual pura para elegir un asiento, debe ser implementada por las clases derivadas
     virtual void elegirAsiento(double costo) = 0;
 };
 
